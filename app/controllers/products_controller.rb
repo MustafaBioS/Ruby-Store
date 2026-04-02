ec8c96f2e21ef17cf31620102c39bda69cc7882c
MustafaBioS <mustaafahany@gmail.com>
2026-04-02 09:19:44 +0200

    ADD - Partials

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,4 +5,30 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+  def new
+    @product = Product.new
+  end
+  def create
+    @product = Product.new(product.name)
+    if @product.save
+      redirect_to @product
+    else
+      render :new, status: :unprocessable_entity
+    end
+    def edit
+      @product = Product.find(params[:id])
+    end
+    def update
+      @product = Product.find(params[:id])
+      if @product.update(product_params)
+        redirect_to @product
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+  end
+  private
+    def product_params
+      params.expect(product: [ :name ])
+    end
 end
